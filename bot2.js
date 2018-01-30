31a59cd7606aaca05b02888e2cea06cdedd95878
mscharlock <michelle.scharlock@gmail.com>
2018-01-30 00:28:13 -0800

    Event listener is working and rendering p's to page

--- bot2.js
+++ bot2.js
@@ -2,26 +2,52 @@
 //Global variables
 const categories = ['drones', 'phones', 'gaming systems', 'computers', 'smart home gadgets', 'wearables'];
 
-const drones = ['drone BEBOP', 'drone BEBOP 2'];
+// const drones = ['drone BEBOP', 'drone BEBOP 2'];
 
-let form = document.getElementById('bot-form');
-let chatHistory = document.getElementById('chat-history');
+let form = document.getElementById('botForm');
+let chatHistory = document.getElementById('chatHistory');
 let userInfo = {
   name: '',
   categoryOfInterest: '',
   productOfInterest: '',
   date: new Date(),
 };
+let firstQ = 'Hello! I am the groverbot! What is your name?';
+
+//Render the first question - fyi, I'm just reminding myself about how to render here
+let kickOffPara = document.createElement('p');
+kickOffPara.innerHTML = firstQ
+chatHistory.appendChild(kickOffPara);
+
+//Form things
+form.addEventListener('submit', formDataName);
+
+function formDataName(event) {
+  event.preventDefault();
+  console.log(event.target.answerBox.value);
+  let nameInput = event.target.answerBox.value;
+  userInfo.name = nameInput
+  let nameChatAnswer = document.createElement('p');
+  nameChatAnswer.innerHTML = nameInput;
+  chatHistory.appendChild(nameChatAnswer);
+  form.reset();
+}
+
+function renderChat() {
+  chatHistory.innerHTML = chatData;
+}
 
 //QUESTIONS
 //Q1
 function initQ() {
-  let firstQ = 'Hello! I am the groverbot! What is your name?';
+  'Hello! I am the groverbot! What is your name?';
   userInfo.name = name;
 
   alert('Nice to meet you, ' + name + '. I will be helping you find a product today. What are you interested in? We have: ' + categories[0] + ' and ' + categories[1] + '. ');
 }
 
+
+initQ();
 //Q2
 // function selectACategory() {
 //   var selectedCategory  = prompt('Tell me, what kind of tech would you be interested in renting from Grover?');
@@ -70,14 +96,3 @@ function initQ() {
 // selectACategory();
 
 //Render dom things
-let chatDiv = document.createElement('div');
-
-
-//Form things
-form.addEventListener('submit', formData)
-
-function formData(event) {
-  event.preventDefault();
-  form.reset();
-
-}
